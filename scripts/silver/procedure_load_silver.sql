4267e872e630354515bc325143547e6b9cb352c5
Amine BELOUH <abelouh2004@gmail.com>
2026-02-03 21:20:01 +0100

    Update procedure_load_silver.sql

--- scripts/silver/procedure_load_silver.sql
+++ scripts/silver/procedure_load_silver.sql
@@ -56,12 +56,16 @@ BEGIN
         cst_key,
         TRIM(cst_firstname) AS cst_firstname,
         TRIM(cst_lastname) AS cst_lastname,
-        CASE WHEN upper(trim(cst_marital_status)) = 'S' THEN 'Single'
+        CASE 
+			 WHEN upper(trim(cst_marital_status)) = 'S' THEN 'Single'
              WHEN upper(trim(cst_marital_status)) = 'M' THEN 'Married'
-             ELSE 'n/a' END AS cst_marital_status, -- Normalize marital status values to readable format
-        CASE WHEN upper(trim(cst_gndr)) = 'F' THEN 'Female'
+             ELSE 'n/a' 
+		END AS cst_marital_status, -- Normalize marital status values to readable format
+        CASE 
+			 WHEN upper(trim(cst_gndr)) = 'F' THEN 'Female'
              WHEN upper(trim(cst_gndr)) = 'M' THEN 'Male'
-             ELSE 'n/a' END AS cst_gndr, -- Normalize gender values to readable format
+             ELSE 'n/a' 
+		END AS cst_gndr, -- Normalize gender values to readable format
         cst_create_date
     FROM (
         SELECT *,
@@ -98,11 +102,13 @@ BEGIN
         SUBSTRING(prd_key, 7, LENGTH(prd_key)) AS prd_key, -- Extract Product Key
         prd_nm,
         COALESCE(prd_cost, 0) AS prd_cost,
-        CASE upper(trim(prd_line)) WHEN 'M' THEN 'Mountain'
-                                   WHEN 'R' THEN 'Road'
-                                   WHEN 'S' THEN 'Other Sales'
-                                   WHEN 'T' THEN 'Touring'
-                                   ELSE 'n/a' END AS prd_line, -- Map Product Line Codes to Descriptive Values
+        CASE upper(trim(prd_line)) 
+		   WHEN 'M' THEN 'Mountain'
+		   WHEN 'R' THEN 'Road'
+		   WHEN 'S' THEN 'Other Sales'
+		   WHEN 'T' THEN 'Touring'
+		   ELSE 'n/a' 
+		END AS prd_line, -- Map Product Line Codes to Descriptive Values
         prd_start_dt::DATE,
         (LEAD(prd_start_dt) OVER (
 			PARTITION BY prd_key ORDER BY prd_start_dt) - INTERVAL '1 day')::DATE AS prd_end_dt -- Calculate End Date as One Day Before Next Start Date
@@ -133,17 +139,31 @@ BEGIN
     SELECT
         sls_ord_num,
         sls_prd_key,
-        sls_cust_id,
-        CASE WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt::TEXT) != 8 THEN NULL ELSE sls_order_dt::TEXT::DATE END AS sls_order_dt,
-        CASE WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt::TEXT) != 8 THEN NULL ELSE sls_ship_dt::TEXT::DATE END AS sls_ship_dt,
-        CASE WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt::TEXT) != 8 THEN NULL ELSE sls_due_dt::TEXT::DATE END AS sls_due_dt,
-        CASE WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price)
-             THEN sls_quantity * ABS(sls_price)
-             ELSE sls_sales END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+        sls_cust_id,	
+        CASE 
+			WHEN sls_order_dt = 0 OR LENGTH(sls_order_dt::TEXT) != 8 THEN NULL 
+			ELSE sls_order_dt::TEXT::DATE 
+		END AS sls_order_dt,
+        CASE 
+			WHEN sls_ship_dt = 0 OR LENGTH(sls_ship_dt::TEXT) != 8 THEN NULL 
+			ELSE sls_ship_dt::TEXT::DATE 
+		END AS sls_ship_dt,
+        CASE 
+			WHEN sls_due_dt = 0 OR LENGTH(sls_due_dt::TEXT) != 8 THEN NULL 
+			ELSE sls_due_dt::TEXT::DATE 
+		END AS sls_due_dt,		
+        CASE 
+			WHEN sls_sales IS NULL OR sls_sales <= 0 OR sls_sales != sls_quantity * ABS(sls_price) THEN sls_quantity * ABS(sls_price)
+            ELSE sls_sales 
+		END AS sls_sales, -- Recalculate sales if original value is missing or incorrect
+		
         sls_quantity,
-        CASE WHEN sls_price IS NULL OR sls_price <= 0
+		
+        CASE 
+			 WHEN sls_price IS NULL OR sls_price <= 0
              THEN sls_sales / NULLIF(sls_quantity, 0)
-             ELSE sls_price END AS sls_price -- Derive price if original value is invalid
+             ELSE sls_price 
+		END AS sls_price -- Derive price if original value is invalid
     FROM bronze.crm_sales_details;
 
     end_time := clock_timestamp();
@@ -167,11 +187,19 @@ BEGIN
   		gen
 	  )
     SELECT
-        CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid)) ELSE cid END AS cid, -- Remove 'NAS' prefix if present
-        CASE WHEN bdate > NOW() THEN NULL ELSE bdate END AS bdate, -- Set future birthdates to NULL
-        CASE WHEN upper(trim(gen)) IN ('F', 'FEMALE') THEN 'Female'
+        CASE 
+			WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LENGTH(cid)) 
+			ELSE cid 
+		END AS cid, -- Remove 'NAS' prefix if present
+        CASE 
+			WHEN bdate > NOW() THEN NULL 
+			ELSE bdate 
+		END AS bdate, -- Set future birthdates to NULL	
+        CASE 
+			 WHEN upper(trim(gen)) IN ('F', 'FEMALE') THEN 'Female'
              WHEN upper(trim(gen)) IN ('M', 'MALE') THEN 'Male'
-             ELSE 'n/a' END AS gen -- Normalize gender values and handle unknown cases
+             ELSE 'n/a' 
+		END AS gen -- Normalize gender values and handle unknown cases
     FROM bronze.erp_cust_az12;
 
     end_time := clock_timestamp();
@@ -191,10 +219,12 @@ BEGIN
 	  )
     SELECT
         REPLACE(cid, '-', '') AS cid,
-        CASE WHEN upper(trim(cntry)) = 'DE' THEN 'Germany'
+        CASE 
+			 WHEN upper(trim(cntry)) = 'DE' THEN 'Germany'
              WHEN upper(trim(cntry)) IN ('US', 'USA') THEN 'United States'
              WHEN trim(cntry) = '' OR cntry IS NULL THEN 'n/a'
-             ELSE trim(cntry) END AS cntry -- Normalize and handle missing or blank country codes
+             ELSE trim(cntry) 
+		END AS cntry -- Normalize and handle missing or blank country codes
     FROM bronze.erp_loc_a101;
 
     end_time := clock_timestamp();
